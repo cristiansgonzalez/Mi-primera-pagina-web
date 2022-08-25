11bbe3b7ce27dcf507937662e032e934eb5fec4a
Leidy <leidyhurtado-1@hotmail.com>
2022-08-24 20:56:53 -0400

    Ejemplo

---
+++ Agroromero/src/main/java/com/example/Agroromero/AgroromeroApplication.java
@@ -0,0 +1,13 @@
+package com.example.Agroromero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class AgroromeroApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(AgroromeroApplication.class, args);
+	}
+
+}
